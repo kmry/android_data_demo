eca2555660c08da2e0e4915a7bb8991d7dc218bb
kmry <kmr_rtf@live.jp>
2016-04-15 19:13:23 +0900

    +OkHttp3

--- app/src/main/java/ncmbdataquick/mbaas/com/nifty/datastorequickstart/MainActivity.java
+++ app/src/main/java/ncmbdataquick/mbaas/com/nifty/datastorequickstart/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,7 +16,31 @@ import com.nifty.cloud.mb.core.NCMB;
 import com.nifty.cloud.mb.core.NCMBException;
 import com.nifty.cloud.mb.core.NCMBObject;
 
+//For OkHttp fig loading part:
+import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import android.util.Log;
+
 public class MainActivity extends AppCompatActivity {
+
+  //For OkHttp fig loading part:
+     private ImageView mImageView;
+     private Button buttonPushMe;
+     private TextView guideText;
+     private TextView NCMBtext;
+     private String baseUrl = "https://scorching-inferno-3735.firebaseapp.com/img/OkhttpTest/IMG_20150510_";
+     private int fig_num =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +48,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
-        //**************** APIキーの設定とSDKの初期化 **********************
-        NCMB.initialize(this, "YOUR_APPLICATION_KEY", "YOUR_CLIENT_KEY");
+
+        //**************** APIキーの設定（自ら記述） **********************
+//        NCMB.initialize(this, "YOUR_APPLICATION_KEY", "YOUR_CLIENT_KEY");
+
+        //**************** OkHttp fig loading part: **********************
+
+        mImageView = (ImageView) findViewById(R.id.image_view);
+
+        guideText = (TextView) findViewById(R.id.guide_text);
+        NCMBtext = (TextView) findViewById(R.id.NCMBtext);
+        buttonPushMe = (Button) findViewById(R.id.btn_pushme);
+        buttonPushMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DownloadImageTask(mImageView)
+                        .execute(baseUrl+ fig_num +".jpg");
+                buttonPushMe.setText("Next!！");
+                guideText.setText(fig_num +"番の画像をダウンロード。");
+                doStartDemo(fig_num);
+                NCMBtext.setText("Saved on NCMB.");
+                fig_num = fig_num +1;
+                if (fig_num >4 ) fig_num =1;
+
+            }
+        });
+
     }
 
     @Override
@@ -50,9 +98,11 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void doStartDemo(View view) {
-        final NCMBObject obj = new NCMBObject("TestClass");
-        obj.put("message", "Hello, NCMB!");
+    private void doStartDemo(int num) {
+        final NCMBObject obj = new NCMBObject("FigSaveLog");
+        obj.put("message", "画像番号"+num);
+        obj.put("fig_id", num);
+
         obj.saveInBackground(new DoneCallback() {
             @Override
             public void done(NCMBException e) {
@@ -65,16 +115,57 @@ public class MainActivity extends AppCompatActivity {
                             .show();
 
                 } else {
-                    //保存成功
+                    /*保存成功
                     new AlertDialog.Builder(MainActivity.this)
                             .setTitle("Notification from Nifty")
-                            .setMessage("Save successfull! with ID:" + obj.getObjectId())
+                             .setMessage("Save successfull! with ID:" + obj.getObjectId())
                             .setPositiveButton("OK", null)
                             .show();
-
+                    */
                 }
             }
         });
 
+    }
+
+    //**************** OkHttp fig loading part: **********************
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+
+            final OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url(urls[0])
+                    .build();
+
+            Response response = null;
+            Bitmap mIcon11 = null;
+            try {
+                response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (response.isSuccessful()) {
+                try {
+                    mIcon11 = BitmapFactory.decodeStream(response.body().byteStream());
+                } catch (Exception e) {
+                    Log.e("Error", e.getMessage());
+                    e.printStackTrace();
+                }
+
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
